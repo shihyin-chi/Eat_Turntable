1878dff4daf576b67f54ec2f308112f77f511c5a
Smallli <ste700789@gmail.com>
2021-09-24 14:22:03 +0800

    Q

--- example/dist/build.js
+++ example/dist/build.js
@@ -2652,7 +2652,7 @@ var Demo = function (_React$Component) {
             null,
             '抽學餐'
           ),
-          _react2.default.createElement(_src2.default, _extends({}, options, { width: 500, height: 500 }))
+          _react2.default.createElement(_src2.default, _extends({}, options, { width: 440, height: 440 }))
         ),
         /*_react2.default.createElement(
           'section',
